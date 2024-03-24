b88f78c8af5d59e8021c8f72650e1b72d18e526d
baskduf <baskduf@gmail.com>
2024-03-24 09:22:58 +0900

    first

---
+++ src/main/java/com/OpenSW/OpenSwApplication.java
@@ -0,0 +1,13 @@
+package com.OpenSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OpenSW/OpenSwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OpenSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
